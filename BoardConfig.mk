3d3dedaf73b34977b057541adc3a3a2e0eb53bec
Steve Kondik <steve@cyngn.com>
2016-04-14 13:53:25 -0700

    s3ve: Enable ZRAM and ZCACHE
     * We have all the necessary bits in msm8226-common kernel now
     * Improves performance over days of usage.
    
    Change-Id: Idc9ea3c18e17f721277893dda2d77d76b71fe07f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_UNIFIED_DEVICE := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37  androidboot.selinux=permissive androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37  androidboot.selinux=permissive androidboot.bootdevice=msm_sdcc.1 zcache.enabled=1
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
