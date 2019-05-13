38a9bebb5e58c6c2f88e9fb232277e6ae18de6a6
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-05-13 22:48:31 +0200

    s3ve: import device specific keylayouts

--- device.mk
+++ device.mk
@@ -43,6 +43,11 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni    
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl 
+    
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
