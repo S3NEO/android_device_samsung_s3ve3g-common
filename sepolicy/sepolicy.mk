7ae44d81aec30aa2d71af8468eb01943f0617cc4
RomanDesigner <romashkarus08@gmail.com>
2020-01-06 14:59:26 +0300

    s3ve3g-common: Add missing separator

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -15,7 +15,7 @@
 #
 
 # Board specific SELinux policy variable definitions
-ifeq($(CONFIG_NEO_SAM_SEPOLICY_TYPE), temporary)
+ifeq ($(CONFIG_NEO_SAM_SEPOLICY_TYPE), temporary)
 BOARD_SEPOLICY_DIRS += \
     device/samsung/s3ve3g-common/sepolicy/temporary
 else
