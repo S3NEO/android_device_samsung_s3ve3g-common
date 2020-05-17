a76a16ac83a73d4d9f5e212258ebf03b77d93f2f
RomanDesigner <romashkarus08@gmail.com>
2020-05-17 14:52:50 +0300

    s3ve3g-common: fix libfmjni name in releasetools script

--- prebuilt/bin/fm.sh
+++ prebuilt/bin/fm.sh
@@ -2,16 +2,16 @@
 # FM Radio fix for Samsung Galaxy S3 Neo
 
 # Check if libfmjni_s3ve3g.so present
-if [ ! -f /system/system/lib/linfmjni_s3ve3g.so ]; then
+if [ ! -f /system/system/lib/libfmjni_s3ve3g.so ]; then
 	echo "There are no libfmjni_s3ve3g.so present" # print error
 	exit 1 # There are no libfmjni_s3ve3g.so present
 fi
 
 # Remove OSS libfmjni (if present)
-rm /system/system/lib/linfmjni.so
+rm /system/system/lib/libfmjni.so
 
 # Rename s3ve3g libfmjni
-mv /system/system/lib/linfmjni_s3ve3g.so /system/system/lib/linfmjni.so
+mv /system/system/lib/libfmjni_s3ve3g.so /system/system/lib/libfmjni.so
 
 # Report successful execution
 exit 0
